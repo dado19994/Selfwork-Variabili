e716508e25880fec7b194ffaedbf937e63a486b0
Davide <dignazio90@gmail.com>
2024-08-13 17:25:48 +0200

    Esercizio Variabili

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+let num1 = 17;
+let num2 = 65;
+
+console.log(num1);
+console.log(num2);
+
+let stringa = 'Ciao a tutti';
+stringa = 'Ciao mi chiamo Davide';
+console.log(stringa);
+
+
+const PIGRECO = 3.14159265359;
+PIGRECO = 3.14
+console.log(PIGRECO);
+
+
